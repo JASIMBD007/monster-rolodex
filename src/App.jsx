479cbd325ff20cd07ac7d612fffe7f49c27d6203
Jasim Uddin <jasim.ice.iu@gmail.com>
2023-08-26 00:11:39 +0200

    some component created and data fetched

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,25 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
 import SearchBox from './components/SearchBox/SearchBox'
 
 function App () {
   const [searchField, setSearchField] = useState('');
-  console.log(searchField)
+  console.log(searchField);
+  const [monsters, setMonsters] = useState([]);
+
+  useEffect('https://jsonplaceholder.typicode.com/users')
+    .then(res => res.json())
+    .then(users => setMonsters(users))
 
   const onSearchChange = (event) => {
     const searchFieldString = event.target.value.toLowerCase();
     setSearchField(searchFieldString);
-  }
+  };
+
+  const filteredMonsters = monsters.filter((monster) => {
+    return monster.name.toLowerCase().includes(searchField);
+  })
+
   return (
     <div>
       <h1 className='app-title'>Monsters Rolodex</h1>
